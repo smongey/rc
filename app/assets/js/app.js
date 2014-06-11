f7d26f93a1b5a51e4e33fa29bef01c6712b5971e
Sean Mongey <sm@smongey.com>
2014-06-11 13:06:09 +0100

    styled audio and added js bits

---
+++ app/assets/js/app.js
@@ -0,0 +1,47 @@
+var video = {
+	credits : $('#credits'),
+	link : $('a#toggle'),
+	isClosed : true,
+	toggle : function(){
+		if (this.isClosed) {
+			console.log('closed');
+			this.isClosed = false;
+			video.credits.fadeOut();
+		} else {
+			console.log('open');
+			this.isClosed = true;
+			video.credits.fadeIn();
+		}
+	}
+
+}
+
+var audio = {
+	link : $('a.open'),
+	toggle : function(event){
+		event.preventDefault();
+		var credits = $(this).siblings('.credits');
+		if (credits.hasClass('open')) {
+			credits.removeClass('open');
+			console.log('close');
+		} else {
+			credits.addClass('open');
+			console.log('open');	
+		}
+	}
+}
+
+
+
+$('#credits').hide();
+
+$(function(){
+
+	video.link.on('click', video.toggle);
+	audio.link.on('click', audio.toggle);
+
+	$('audio,video').mediaelementplayer({
+		features: ['playpause','progress']
+	});
+
+});
